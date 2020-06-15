938c6fcabd66fbd9e675293e454bd47097c7db4e
Dawn Yue <1179348863@qq.com>
2020-06-15 14:51:50 +0800

    ycx

--- Fire_fighting1/Fire_fighting1.cpp
+++ Fire_fighting1/Fire_fighting1.cpp
@@ -8,94 +8,10 @@ using namespace std;
 //水柱的轨迹需要先用背景差分获得水柱的连通域，然后利用连通域上的像素点进行曲线的拟合
 //水枪的位置视为已知，即可以手动活动坐标。
 
-
 //识别并标出火焰
-//输入 原图像 输出：原图像上框出框框（火）
-void find_fire(Mat& srcMat, Mat& outputMat)
-{
-	Mat gray_srcMat;
-	Mat dstMat, binMat;
-	cvtColor(srcMat, gray_srcMat, COLOR_BGR2GRAY);
-	Mat fire_Mat = Mat::zeros(gray_srcMat.size(), gray_srcMat.type());
-	cvtColor(srcMat, dstMat, COLOR_BGR2HSV);
-	vector<Mat> channels;
-	split(dstMat, channels);
-	//将S通道的图像复制，然后处理
-	Mat S_Mat;
-	channels.at(1).copyTo(S_Mat);
-	int row_num = srcMat.rows;            //行数
-	int col_num = srcMat.cols;            //列数
-	//双重循环，遍历右下角像素值
-	for (int i = row_num * 0.75; i < row_num; i++)    //行循环
-	{
-		for (int j = col_num * 0.75; j < col_num; j++)    //列循环
-		{
-			//-------【开始处理每个像素】---------------
-			if ((gray_srcMat.at<uchar>(i, j) >= 150 && S_Mat.at<uchar>(i, j) >= 120))
-			{
-				fire_Mat.at<uchar>(i, j) = 255;
-			}
-			//-------【处理结束】---------------
-		}
-	}
-	//膨胀
-	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));    //返回的是内核矩阵
-	Mat firedstImage;
-	dilate(fire_Mat, fire_Mat, element);
-	//通过findContours函数寻找连通域
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-	findContours(fire_Mat, contours, RETR_LIST, CHAIN_APPROX_NONE);
-	//绘制轮廓
-	for (int i = 0; i < contours.size(); i++) {
-		RotatedRect rbox = minAreaRect(contours[i]);
-		drawContours(srcMat, contours, i, Scalar(0, 255, 255), 1, 8);
-		Point2f vtx[4];
-		rbox.points(vtx);
-		for (int j = 0; j < 4; ++j) {
-			cv::line(outputMat, vtx[j], vtx[j < 3 ? j + 1 : 0], Scalar(255, 255, 255), 2, LINE_AA);
-		}
-	}
-}
-
-
+void fire(Mat& srcMat, Mat& outputMat);
 //曲线拟合函数，因上课无曲线拟合内容，参考CSDN博客： https://blog.csdn.net/guduruyu/article/details/72866144
-bool polynomial_curve_fit(std::vector<cv::Point>& key_point, int n, cv::Mat& A)
-{
-	//Number of key points
-	int N = key_point.size();
-
-	//构造矩阵X
-	cv::Mat X = cv::Mat::zeros(n + 1, n + 1, CV_64FC1);
-	for (int i = 0; i < n + 1; i++)
-	{
-		for (int j = 0; j < n + 1; j++)
-		{
-			for (int k = 0; k < N; k++)
-			{
-				X.at<double>(i, j) = X.at<double>(i, j) +
-					std::pow(key_point[k].x, i + j);
-			}
-		}
-	}
-
-	//构造矩阵Y
-	cv::Mat Y = cv::Mat::zeros(n + 1, 1, CV_64FC1);
-	for (int i = 0; i < n + 1; i++)
-	{
-		for (int k = 0; k < N; k++)
-		{
-			Y.at<double>(i, 0) = Y.at<double>(i, 0) +
-				std::pow(key_point[k].x, i) * key_point[k].y;
-		}
-	}
-
-	A = cv::Mat::zeros(n + 1, 1, CV_64FC1);
-	//求解矩阵A
-	cv::solve(X, Y, A, cv::DECOMP_LU);
-	return true;
-}
-
+bool curve_fit(std::vector<cv::Point>& key_point, int n, cv::Mat& A);
 int main()
 {
 	VideoCapture capVideo(VIDEO_PATH);
@@ -124,16 +40,20 @@ int main()
 			firemat = frame.clone();
 			firemat1 = frame.clone();
 		}
+		if (cnt == 100) {
+			firemat = frame.clone();
+			firemat1 = frame.clone();
+		}
 
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
 		if (cnt == 0) {
 			frame.copyTo(bgMat);//获得背景图像
-			find_fire(firemat, firemat);
+			fire(firemat, firemat);
 			dst = frame.clone();//备份
 		}
 		else if (cnt == 100) {
 			frame.copyTo(bgMat);//更新背景图像
-			find_fire(firemat, firemat);
+			fire(firemat, firemat);
 			imshow("output", firemat);
 			dst = frame.clone();//更新备份
 		}
@@ -172,7 +92,7 @@ int main()
 		
 			//2.绘制折线
 			if ((cnt >= 104)&& (cnt <= 145)) {
-				polynomial_curve_fit(points, 3, A);
+				curve_fit(points, 3, A);
 				std::cout << "A = " << A << std::endl;
 				std::vector<cv::Point> points_fitted;
 				for (int x = 170; x < 432; x++)//水枪到火焰
@@ -182,10 +102,8 @@ int main()
 					points_fitted.push_back(cv::Point(x, y));
 				}
 				cv::polylines(firemat, points_fitted, false, cv::Scalar(255, 0, 0), 1, 8, 0);
-
 			}
-			
-			imshow("firemat);", firemat);
+			imshow("firemat", firemat);
 			imshow("frame", frame);
 			//imshow("dst", dst);
 			waitKey(30);
@@ -193,4 +111,93 @@ int main()
 		cnt++;
 	}
 	return 0;
+}
+
+//识别并标出火焰
+void fire(Mat& srcMat, Mat& outputMat)
+{
+	Mat gray;
+	Mat hsvMat;
+	cvtColor(srcMat, gray, COLOR_BGR2GRAY);
+	Mat dstMat = Mat::zeros(gray.size(), gray.type());
+	cvtColor(srcMat, hsvMat, COLOR_BGR2HSV);
+	vector<Mat> channels;
+	//分离通道
+	split(hsvMat, channels);
+
+	//Mat H_Mat;
+	Mat S_Mat;
+	//Mat V_Mat;
+	//channels.at(0).copyTo(H_Mat);
+	channels.at(1).copyTo(S_Mat);
+	//channels.at(2).copyTo(V_Mat);
+	//imshow("H", H_Mat);
+	//imshow("S", S_Mat);
+	//imshow("V", V_Mat);
+
+	//分离HSV通道三个得出S通道最适合进行处理m，并且发现火焰一直处于右下角，故遍历像素只需右下角即可
+
+	//遍历像素
+	int row = srcMat.rows;            //行数
+	int col = srcMat.cols;            //列数
+	for (int i = row * 0.75; i < row; i++)    //行循环
+	{
+		for (int j = col * 0.75; j < col; j++)    //列循环
+		{
+			//开始处理每个像素
+			if ((gray.at<uchar>(i, j) >= 150 && S_Mat.at<uchar>(i, j) >= 120))
+			{
+				dstMat.at<uchar>(i, j) = 255;
+			}
+			//处理结束
+		}
+	}
+	//膨胀处理
+	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));    //定义内核矩阵，尺寸为15 15的矩形
+	dilate(dstMat, dstMat, element);//膨胀
+
+	//通过findContours函数寻找连通域
+	vector<vector<Point>> contours;
+	findContours(dstMat, contours, RETR_LIST, CHAIN_APPROX_NONE);
+
+	//描绘轮廓
+	for (int i = 0; i < contours.size(); i++) {
+		drawContours(srcMat, contours, i, Scalar(0, 0, 255), 1, 8);
+	}
+}
+//曲线拟合函数，因上课无曲线拟合内容，参考CSDN博客： https://blog.csdn.net/guduruyu/article/details/72866144
+bool curve_fit(std::vector<cv::Point>& key_point, int n, cv::Mat& A)
+{
+	//Number of key points
+	int N = key_point.size();
+
+	//构造矩阵X
+	cv::Mat X = cv::Mat::zeros(n + 1, n + 1, CV_64FC1);
+	for (int i = 0; i < n + 1; i++)
+	{
+		for (int j = 0; j < n + 1; j++)
+		{
+			for (int k = 0; k < N; k++)
+			{
+				X.at<double>(i, j) = X.at<double>(i, j) +
+					std::pow(key_point[k].x, i + j);
+			}
+		}
+	}
+
+	//构造矩阵Y
+	cv::Mat Y = cv::Mat::zeros(n + 1, 1, CV_64FC1);
+	for (int i = 0; i < n + 1; i++)
+	{
+		for (int k = 0; k < N; k++)
+		{
+			Y.at<double>(i, 0) = Y.at<double>(i, 0) +
+				std::pow(key_point[k].x, i) * key_point[k].y;
+		}
+	}
+
+	A = cv::Mat::zeros(n + 1, 1, CV_64FC1);
+	//求解矩阵A
+	cv::solve(X, Y, A, cv::DECOMP_LU);
+	return true;
 }
